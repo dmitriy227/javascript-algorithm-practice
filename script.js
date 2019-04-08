688771343f7fb32ff309eee2fc7ddc7d61c2f632
Dmitriy Zheltkov <dzzy2twenty@gmail.com>
2019-04-08 03:20:04 -0700

    Completed Algo: Reverse a String

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 //// Convert Celsius to Fahrenheit ////
 
 function convertToF(celsius) {
-    let fahrenheit = celsius * (9/5) + 32;
-    return fahrenheit;
-  }
+  let fahrenheit = celsius * (9/5) + 32;
+  return fahrenheit;
+}
   
-  convertToF(30);
+convertToF(30);
 
-  
+//// Reverse a String ////
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString("hello");
